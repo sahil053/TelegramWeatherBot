eb4ccc963838413506b7b0a78c3ae55af35f9d81
sahil053 <sahilsinghiscool@gmail.com>
2024-01-05 19:37:44 +0530

    few changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,8 +5,14 @@ export default function Footer() {
     <div className='flex justify-center my-5'>
       <a className='w-fit hover:underline hover:opacity-80 flex items-center gap-2 text-text' href='https://github.com/sahil053/TelegramWeatherBot' rel='noreferrer' target='_blank'>
         <img src={GitIcon} alt='Github' />
-        <p className='text-sm'>Sahil Singh</p>
+        <p className='text-sm'>Repo Link|-----</p>
       </a>
+
+      <a className='w-fit hover:underline hover:opacity-80 flex items-center gap-2 text-text' href='https://t.me/weather53bot' rel='noreferrer' target='_blank'>
+       
+        <p className='text-sm'>   ----|Telegram Bot Link</p>
+      </a>
+      
     </div>
   )
 }
