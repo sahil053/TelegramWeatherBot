cb6d031f642a8f2179395253de62012569f0465f
sahil053 <sahilsinghiscool@gmail.com>
2024-01-05 18:38:02 +0530

    made few changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import GitIcon from '../assets/github.svg'
 export default function Footer() {
   return (
     <div className='flex justify-center my-5'>
-      <a className='w-fit hover:underline hover:opacity-80 flex items-center gap-2 text-text' href='https://github.com/sahil053' rel='noreferrer' target='_blank'>
+      <a className='w-fit hover:underline hover:opacity-80 flex items-center gap-2 text-text' href='https://github.com/sahil053/TelegramWeatherBot' rel='noreferrer' target='_blank'>
         <img src={GitIcon} alt='Github' />
         <p className='text-sm'>Sahil Singh</p>
       </a>
